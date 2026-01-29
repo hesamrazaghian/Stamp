1ffceba0c164f27d187a628d79611d10d23a8da8
Hamid Mayeli <hamid.mayeli@yahoo.com>
2026-01-29 21:14:15 +0000

    Cleanup

--- Stamp.Domain/Enums/RoleEnum.cs
+++ Stamp.Domain/Enums/RoleEnum.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Stamp.Domain.Enums;
 
-namespace Stamp.Domain.Enums
+public enum RoleEnum
 {
-    public enum RoleEnum
-    {
-        Guest = 0,
-        User = 1,
-        Admin = 2
-    }
+    Guest = 0,
+    User = 1,
+    Admin = 2
 }
